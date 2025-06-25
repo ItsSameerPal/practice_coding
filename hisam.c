308caeebf1e1f27b06b66e70b152de9a2f29bdba
SAMEER <this.sameer9@gmail.com>
2025-06-26 01:25:07 +0530

    added function for sum of two digits

--- hisam.c
+++ hisam.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int sum(int a,int b){
+    return a+b;
 }
+
+int main() {
+    // printf("Hello, World!\n");
+    // return 0;
+    int res= sum(3,4);
+    printf(res);
+}
+
